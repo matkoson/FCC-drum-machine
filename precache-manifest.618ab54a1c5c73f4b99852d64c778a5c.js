9b94ca1a73e3a920771932eed2db37284ecb35f4
Mateusz Kosoń <byconradsong@gmail.com>
2019-03-23 02:09:30 +0100

    Updates

---
+++ precache-manifest.618ab54a1c5c73f4b99852d64c778a5c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/FCC-drum-machine/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "e3c1e9d5d28c8fc780c5",
+    "url": "/FCC-drum-machine/static/css/main.36d7688b.chunk.css"
+  },
+  {
+    "revision": "2d570afc4052efe196a8",
+    "url": "/FCC-drum-machine/static/js/runtime~main.432d2457.js"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/FCC-drum-machine/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "0a8d03ac917fff5cbb9b",
+    "url": "/FCC-drum-machine/static/js/2.4379860c.chunk.js"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/FCC-drum-machine/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/FCC-drum-machine/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/FCC-drum-machine/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/FCC-drum-machine/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/FCC-drum-machine/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "e3c1e9d5d28c8fc780c5",
+    "url": "/FCC-drum-machine/static/js/main.34244492.chunk.js"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/FCC-drum-machine/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/FCC-drum-machine/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/FCC-drum-machine/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/FCC-drum-machine/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/FCC-drum-machine/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/FCC-drum-machine/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/FCC-drum-machine/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/FCC-drum-machine/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "0a8d03ac917fff5cbb9b",
+    "url": "/FCC-drum-machine/static/css/2.3a5c7c42.chunk.css"
+  },
+  {
+    "revision": "6adb63ff30187b9a53ca985191f17f4c",
+    "url": "/FCC-drum-machine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6960739b5a1d9a385d752db6116953df.js"
+  "/FCC-drum-machine/precache-manifest.618ab54a1c5c73f4b99852d64c778a5c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/FCC-drum-machine/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
